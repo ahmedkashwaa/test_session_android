3121682fb1159eabc3f3f052a0010a2ff56c1697
ahmedkachwaa <ahmedkashwaa@hotmail.com>
2025-10-29 20:15:22 +0300

    test

--- app/src/main/java/com/example/new_app/MainActivity.kt
+++ app/src/main/java/com/example/new_app/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         
         setContent {
+            
             New_appTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
